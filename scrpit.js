a171ed1110a1d4a702e12e63240a2d7c3391f513
Shah sawar <shahsawarsawar346@gmail.com>
2025-12-14 15:01:22 +0500

    Fix password generation logic and event listener

--- scrpit.js
+++ scrpit.js
@@ -1,18 +1,15 @@
 const chars = "awefaslhasuf3276kjsadf8jkkj#%&^Bhjghj%$$ASjknbm*%76&^BACh7*A^*&JGda%&Â5dNMBAc*&^SA";
-let password = "";
 const passLength = 10;
 
 function getpassword() {
+    let password = "";
 
     for (let i = 0; i < passLength; i++) {
         const rndIndex = Math.floor(Math.random() * chars.length);
-        password += chars.substring(rndIndex,rndIndex +-3);
+        password += chars[rndIndex];
     }
 
     document.querySelector("#Passwords").value = password;
-    
 }
 
-document.querySelector("#btn").addEventListener("click", function () {
-    document.querySelector("#Passwords").value = "";
-})
+document.querySelector("#btn").addEventListener("click", getpassword);
